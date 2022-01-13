dae3fd933ded792169cdf32864714f5f4bef779d
manan-shxrma <manansharmams3@gmail.com>
2022-01-13 19:43:09 +0530

    optimised home screen

--- src/components/body.js
+++ src/components/body.js
@@ -1,15 +1,21 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import ParticleBack from "./particleback";
 import Typewriter from 'typewriter-effect';
 import Model2 from "./model2"
 import Button from "./stylebutton"
+import RingLoader from "react-spinners/RingLoader";
+import "../css/index.css"
 
-
-
-function body(){
-   
+function Body(){
+  const [loading,setLoading]=useState(true);
+            useEffect(()=>{
+       setLoading(true)
+       setTimeout(()=>{
+          setLoading(false) 
+       },400000)
+  },[])
        return(<>
-         <h1 style={{"textAlign": "center","marginTop": "5rem"}}>
+         <h1 className="typewriter" style={{textAlign: "center","marginTop": "5rem"}}>
               <Typewriter
                 options={{
                   strings: ['नमस्ते, इस खूबसूरत ब्रह्मांड में आपका स्वागत है','Hello, Welcome to this beautiful universe' ,'This universe is full of questions','यह ब्रह्मांड सवालों से भरा है','आइए, जानते हैं कुछ सवालों के जवाब' ,'Let us ponder over a few of them'],
@@ -19,9 +25,14 @@ function body(){
                 }}
               /></h1>
            <ParticleBack></ParticleBack>
-           <Model2 />
+           { loading?
+                <div className="preloaderhome">
+                <RingLoader color="#fff" loading={loading}  size={100} />
+                 </div>
+                :
+                  <Model2 />}
            <Button />
        </>
        )}
 
-export default body
+export default Body
